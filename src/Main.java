cb600ec1619f39daf0ae16aae04f998eae2d36b4
admin <valery.muhanin@yandex.ru>
2025-06-05 21:26:59 +0300

    Повторение мать учения!

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+    public static void main(String[] args) {
+        PersonService personService = new PersonService();
+        Person sarah = new Person("Sarah ", 30);
+        if (sarah.child != null) {
+            System.out.println("sarah.child.getName() = " + sarah.child.getName());
+        }else{
+            System.out.println("No child");
+        }
+        Person john = new Person("John", 13);
+        sarah.child = john;
+
+        personService.setAgeToEighteen(sarah);
+        if (sarah.child != null) {
+            if (sarah.child.getName() != null) {
+                System.out.println("sarah.child.getName().length() = " + sarah.child.getName().length());
+            }else{
+                System.out.println(0);
+            }
+        System.out.println("sarah.getAge() = " + sarah.getAge());
+        john = new Person("Alternativ John", 30);
+        john = null;
+        personService.changePersonToDani(sarah);
+        System.out.println("sarah.child.getAge() = " + sarah.child.getName());
+        // System.out.println("john.getName() = " + john.getName());
+        System.out.println("sarah.getAge() = " + sarah.getAge());
+
+    }
+}
+}
---
+++ src/PersonService.java
@@ -0,0 +1,16 @@
+public class PersonService {
+    public void setAgeToEighteen(Person person) {
+        person.setAge(18);
+        person.child = new Person(null, 30);
+
+    }
+
+    public void changePersonToDani(Person person) {
+        person = new Person("Dani ", 26);
+        Person padme = new Person("Padme ", 26);
+        Person luke = new Person("Luke ", 19);
+        padme.child = luke;
+        luke.parent = padme;
+
+    }
+}
